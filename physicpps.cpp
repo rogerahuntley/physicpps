0f139913aa810f59a3d123ea1206d66c07725ffb
Roger II <roger.a.huntley@gmail.com>
2021-03-01 22:59:27 -0500

    starting to write basic collision detection code

--- physicpps.cpp
+++ physicpps.cpp
@@ -1,1 +1,24 @@
 #include "physicpps.h"
+
+namespace physicpps {
+
+static bool rectCollide(rectangle& rect1, rectangle& rect2){
+    // check collistion, return if true
+    if (rect1.x < rect2.x + rect2.width &&  // check left side
+    rect1.x + rect1.width > rect2.x &&      // check right
+    rect1.y < rect2.y + rect2.height &&     // check up
+    rect1.y + rect1.height > rect2.y) {     // check down
+        return true;
+    } else {
+        // else, return false
+        return false;
+    }
+};
+
+
+
+
+
+
+
+};
--- physicpps.h
+++ physicpps.h
@@ -10,21 +10,15 @@ struct rectangle {
     float y;
     float width;
     float height;
+    rectangle(int x = 0, int y = 0, int width = 0, int height = 0) : x(x), y(y), width(width), height(height) {};
 };
 
-static bool rectCollide(rectangle& rect1, rectangle& rect2){
-    // check collistion, return if true
-    if (rect1.x < rect2.x + rect2.width &&  // check left side
-    rect1.x + rect1.width > rect2.x &&      // check right
-    rect1.y < rect2.y + rect2.height &&     // check up
-    rect1.y + rect1.height > rect2.y) {     // check down
-        return true;
-    } else {
-        // else, return false
-        return false;
-    }
-};
+static bool rectCollide(rectangle& rect1, rectangle& rect2);
 
+struct Vector2 {
+    float x;
+    float y;
+};
 
 
 
